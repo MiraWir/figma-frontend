1aca9637e3ac2a0aa1c1d97824d4de421f4f6ac7
Miray <99430985+MiraWir@users.noreply.github.com>
2026-05-08 01:14:48 +0300

    hadi bakalım hayirlisi

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        background: "#FFB9AF",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "32px",
+        fontWeight: "bold",
+      }}
+    >
+      APP IS WORKING
+    </div>
+  );
+}
